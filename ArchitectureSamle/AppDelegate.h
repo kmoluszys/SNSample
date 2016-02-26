d7d1a84a3bf75ac56bae329f51bc948303dfd2ea
Karol Moluszys <kmoluszys@speednet.pl>
2016-02-26 09:40:53 +0100

    Initial Commit

---
+++ ArchitectureSamle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArchitectureSamle
+//
+//  Created by Karol Moluszys on 26.02.2016.
+//  Copyright © 2016 Karol Moluszys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArchitectureSamle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArchitectureSamle
+//
+//  Created by Karol Moluszys on 26.02.2016.
+//  Copyright © 2016 Karol Moluszys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
